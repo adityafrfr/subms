a9fa9eb2e825bb25c098f0af48e169a1560a52ee
Aditya <mail4adityaa@gmail.com>
2026-05-26 06:34:14 +0530

    3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 
 app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+app.use(morgan('dev'))
 
 let people = [{
         "id": "1",
@@ -94,6 +106,12 @@ app.post('/api/persons', (request, response) => {
     return response.status(201).json(person)
 
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const port = 3001
 app.listen(port, () => {
