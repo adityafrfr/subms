579efe181f9151a4abcb5ab322ba1047c690c7cc
Aditya <mail4adityaa@gmail.com>
2026-06-06 13:54:12 +0530

    start of a massive refactor

---
+++ part4/NotesApp/frontend/src/services/notes.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const baseUrl = '/api/notes'
+
+const getAll = () => {
+    const response = axios.get(baseUrl)
+    return response.then(response => response.data)
+}
+
+const create = (newObject) => {
+    const response = axios.post(baseUrl, newObject)
+    return response.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+    const response = axios.put(baseUrl + `/${id}`, newObject)
+    return response.then(response =>response.data)
+}
+
+export default { getAll, create, update }
